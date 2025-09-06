19ae500f68c8f61c3d3ee72e1d739ee93906e259
asishpradhan01 <153635662+asishpradhan01@users.noreply.github.com>
2025-09-06 19:06:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,16 +18,17 @@ document.querySelector(".my-txt").innerHTML = data.main.humidity + "%";
 document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
 
   if(data.weather[0].main == "Clouds") {
-        icons.src = "./images/cloudy.png";
+        icons.src = "cloudy.png";
   } else if(data.weather[0].main == "cloud") {
-    icons.src = "./images/cloud.png";
+    icons.src = "cloud.png";
   } else if(data.weather[0].main == "rain") {
-    icons.src = "./images/rain.png";
+    icons.src = "rain.png";
   }
 }
 
 searchbtn.addEventListener("click", () => {
     checkWhether(searchbox.value);
 })
+
 
 
